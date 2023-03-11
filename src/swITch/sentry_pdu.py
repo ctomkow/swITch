f815eeba76e94952cc14c9f724922aa33d73f5ca
ctomkow <ctomkow@gmail.com>
2023-03-10 16:42:15 -0800

    another fix for pdus

--- src/swITch/sentry_pdu.py
+++ src/swITch/sentry_pdu.py
@@ -13,7 +13,7 @@ def _override_check_dsa_parameters(parameters):
     Allows for shorter or longer parameters.p to be returned from the server's host key. This is a
     HORRIBLE hack and a security risk, please remove if possible!
     """
-    if parameters.p.bit_length() not in [640, 1024, 2048, 3072, 4096]:
+    if parameters.p.bit_length() not in [640, 641, 1024, 2048, 3072, 4096]:
         raise ValueError("p is {}, must be exactly 640, 1024, 2048, 3072, or 4096 bits long".format(parameters.p.bit_length()))
     if parameters.q.bit_length() not in [160, 256]:
         raise ValueError("q must be exactly 160 or 256 bits long")
